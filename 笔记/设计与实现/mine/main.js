4b3e854836715677b628b7ab97bf264a46332f4e
wanjingzhang <95616085@qq.com>
2023-04-25 11:21:49 +0800

    feat: add component type for render function

--- 笔记/设计与实现/mine/main.js
+++ 笔记/设计与实现/mine/main.js
@@ -1,38 +1,59 @@
 // 渲染obj的渲染器
 // 渲染函数，传入对象，渲染出数据
-function Render(obj, root) {
-  let el = document.createElement(obj.tag)
+function mountComponent(vnode, container) {
+  let subtree = vnode.tag()
+  Render(subtree, container)
+}
+
+function mountElement(vnode, container) {
+  let el = document.createElement(vnode.tag)
   // 添加事件
-  for (let key in obj.props) {
+  for (let key in vnode.props) {
     if (/^on/.test(key)) {
       el.addEventListener(
         key.substring(2, key.length).toLowerCase(),
-        obj.props[key]
+        vnode.props[key]
       )
     }
   }
-  if (typeof obj.children == 'string') {
-    let text = document.createTextNode(obj.children)
+  if (typeof vnode.children == 'string') {
+    let text = document.createTextNode(vnode.children)
     el.appendChild(text)
-  } else if (obj.children) {
-    obj.children.forEach(child => Render(child, el))
+  } else if (vnode.children) {
+    vnode.children.forEach(child => Render(child, el))
   }
-  root.appendChild(el)
+  container.appendChild(el)
 }
 
-const obj = {
-  tag: 'div',
-  children: [
-    {
-      tag: 'span',
-      children: 'hello world',
-      props: {
-        onClick: () => {
-          alert('hello')
+function Render(vnode, container) {
+  // 判断它是组件还是普通元素
+  if (typeof vnode.tag == 'string') {
+    mountElement(vnode, container)
+  } else if (typeof vnode.tag == 'function') {
+    mountComponent(vnode, container)
+  }
+}
+
+function MyComponent() {
+  let obj = {
+    tag: 'div',
+    children: [
+      {
+        tag: 'span',
+        children: 'hello world',
+        props: {
+          onClick: () => {
+            alert('hello')
+          }
         }
       }
-    }
-  ]
+    ]
+  }
+  return obj
 }
 
-Render(obj, document.body)
+const vnode = {
+  tag: MyComponent
+}
+
+Render(vnode, document.body)
